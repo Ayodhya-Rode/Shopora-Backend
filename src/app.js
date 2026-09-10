2f1b1bc732f22870d4fe3a0c1af194b8742fcdba
Ayodhya Rode <ayodhyarode168@gmail.com>
2026-09-10 15:10:15 +0530

    fix: localhost url

--- src/app.js
+++ src/app.js
@@ -21,7 +21,6 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 const allowedOrigins = [
-  "http://localhost:5173",
   "https://shopora-frontend-wxiy.onrender.com",
 ];
 
